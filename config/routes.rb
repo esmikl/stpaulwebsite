d3621dd5cf64966c2f7e3be9990757184c8310b2
Emily Mikl <emilysmikl@gmail.com>
2016-01-30 14:14:57 -0600

    Trying to fix rake error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   get '/about', to: 'welcome#about', as: 'about'
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
